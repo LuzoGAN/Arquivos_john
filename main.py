644092055f2a529380d03322fca71e579ccaf508
Luzo Gomes <luzoaki@gmail.com>
2024-07-05 17:00:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -51,4 +51,5 @@ def upload_file():
         return "Formato de arquivo não suportado"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
